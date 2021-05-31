e7c3f8889619ee80a3b94e67eace7fe20676377a
Alessandro MONNO <alessandro.monno@alten.it>
2021-05-31 14:50:03 +0200

    rimosso codice dubbio (in .net 5 ci sono stati molti fix)

---
+++ mistakes/CodingMistakes.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CodingMistakes.Memory
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly Dictionary<Guid, string> example = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrossThread_Click(object sender, RoutedEventArgs e)
+        {
+            DeadWindow window = new DeadWindow();
+            window.Show();
+        }
+
+        private async void btnBadAsync_Click(object sender, RoutedEventArgs e)
+        {
+            this.AsyncCorrect();
+            this.AsyncCorrect();
+        }
+
+        private async void AsyncVoid()
+        {
+            this.example.Add(Guid.NewGuid(), "test");
+        }
+
+        private Task AsyncCorrect()
+        {
+            return Task.Run(() => this.example.Add(Guid.NewGuid(), "test"));
+        }
+    }
+}
